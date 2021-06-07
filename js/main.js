b70943113662ee7f07deb9fba0c9cf8728a62c0b
mopiw <rche@foxmail.com>
2021-06-07 22:44:30 +0800

    fix: cusdis app id error

--- js/main.js
+++ js/main.js
@@ -1,62 +1,61 @@
 // dark mode
 
-let getLocalStorage = localStorage.getItem("theme-mode");
-let html = document.querySelector("html");
-let getHtmlThemeMode = html.getAttribute("data-theme-mode");
-const themeModeSwitch = document.querySelectorAll(".theme-mode-switch-icon");
+let getLocalStorage = localStorage.getItem('theme-mode')
+let html = document.querySelector('html')
+let getHtmlThemeMode = html.getAttribute('data-theme-mode')
+const themeModeSwitch = document.querySelectorAll('.theme-mode-switch-icon')
 
 function themeMode(mode, onload) {
   if (onload) {
-    html.setAttribute("data-theme-mode", getLocalStorage);
-    localStorage.setItem("theme-mode", getLocalStorage);
+    html.setAttribute('data-theme-mode', getLocalStorage)
+    localStorage.setItem('theme-mode', getLocalStorage)
     window.onload = function () {
       window.CUSDIS.setTheme(getLocalStorage)
     }
-  } else if (mode != "") {
-    html.setAttribute("data-theme-mode", mode);
-    localStorage.setItem("theme-mode", mode);
+  } else if (mode != '') {
+    html.setAttribute('data-theme-mode', mode)
+    localStorage.setItem('theme-mode', mode)
     window.CUSDIS.setTheme(mode)
   }
 }
 
-if (getLocalStorage === "dark") {
+if (getLocalStorage === 'dark') {
   themeMode(getLocalStorage, true)
 } else if (getLocalStorage === null) {
-  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    themeMode("dark", true)
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    themeMode('dark', true)
   }
 }
 
 window
-  .matchMedia("(prefers-color-scheme: dark)")
-  .addEventListener("change", function (e) {
+  .matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', function (e) {
     if (e.matches) {
-      themeMode("dark");
+      themeMode('dark')
     } else {
-      themeMode("light");
+      themeMode('light')
     }
-  });
+  })
 
 for (const element of themeModeSwitch) {
-  element.addEventListener("click", function (e) {
-    if (this.classList[1] == "to-dark") {
-      themeMode("dark");
-    } else if (this.classList[1] == "to-light") {
-      themeMode("light");
+  element.addEventListener('click', function (e) {
+    if (this.classList[1] == 'to-dark') {
+      themeMode('dark')
+    } else if (this.classList[1] == 'to-light') {
+      themeMode('light')
     }
-  });
+  })
 }
 
-
 // posts images full width
-const postP = document.querySelectorAll(".post-content p");
-const postWidth = document.querySelector(".post-content").offsetWidth;
+const postP = document.querySelectorAll('.post-content p')
+const postWidth = document.querySelector('.post-content').offsetWidth
 
 for (const element of postP) {
-  if (element.querySelector("img")) {
-    element.classList.add("has-img");
-    if (element.querySelector("img").offsetWidth >= postWidth) {
-      element.classList.add("img-full");
+  if (element.querySelector('img')) {
+    element.classList.add('has-img')
+    if (element.querySelector('img').offsetWidth >= postWidth) {
+      element.classList.add('img-full')
     }
   }
 }
@@ -77,8 +76,8 @@ const prvs = new Parvus()
 
 // back top top
 window.onscroll = function () {
-  let top = document.documentElement.scrollTop;
-  const backToTop = document.querySelector('.back-to-top');
+  let top = document.documentElement.scrollTop
+  const backToTop = document.querySelector('.back-to-top')
 
   if (top >= 600) {
     backToTop.style.opacity = '1'
